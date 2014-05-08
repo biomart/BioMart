f9b82cbb975f35f25f6cb55f7752fa27d09cee14
lpand <luca1.pandini@gmail.com>
2014-05-08 14:39:21 +0200

    watch species param changes

--- plugins/enrichment/public/enrichment/mart-visual-enrichment/app/js/controllers/species.js
+++ plugins/enrichment/public/enrichment/mart-visual-enrichment/app/js/controllers/species.js
@@ -7,14 +7,19 @@ var app = angular.module("martVisualEnrichment.controllers");
 app.controller("SpeciesCtrl", ["$scope", "$location", "$route", "queryStore",
     function SpeciesCtrl($scope, $loc, $route, qs) {
 
-    var sName = $loc.search().species, species;
-    for (var i = 0, len = $scope.species.length; i < len; ++i) {
-        species = $scope.species[i];
-        if (species.name === sName) {
-            $scope.selectedSpecies = species;
-            break;
+    var sName = $loc.search().species;
+
+    this.getSpeciesByName = function (name) {
+        for (var i = 0, len = $scope.species.length, species = ""; i < len; ++i) {
+            species = $scope.species[i];
+            if (species.name === name) {
+                return species;
+            }
         }
-    }
+
+        throw new Error("Cannot find species with name "+ name);
+    };
+
     this.updateSpecies = function (species) {
         var s = $loc.search(), newS = {};
         newS.config = s.config;
@@ -27,6 +32,15 @@ app.controller("SpeciesCtrl", ["$scope", "$location", "$route", "queryStore",
     };
 
     $scope.updateSpecies = this.updateSpecies;
+    $scope.selectedSpecies = this.getSpeciesByName(sName);
+
+    $scope.$on("$routeUpdate", function () {
+        var species = $loc.search().species;
+
+        if (species !== $scope.selectedSpecies.name) {
+            $scope.selectedSpecies = this.getSpeciesByName(species);
+        }
+    }.bind(this));
 
 }]);
 
