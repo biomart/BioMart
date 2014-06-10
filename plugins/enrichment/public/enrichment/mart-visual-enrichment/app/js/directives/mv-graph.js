c1b16c4050b0da282802eb13e483b64ab5b84993
Luca Pandini <luca1.pandini@gmail.com>
2014-06-10 13:52:51 +0200

    symulation time limit; include genes while filtering

--- plugins/enrichment/public/enrichment/mart-visual-enrichment/app/js/directives/mv-graph.js
+++ plugins/enrichment/public/enrichment/mart-visual-enrichment/app/js/directives/mv-graph.js
@@ -234,6 +234,7 @@ var graph = (function (d3) {
         }
       });
 
+    var startTick = +new Date(), symTime = 5000;
     var force = d3.layout.force()
       .size([width, height])
       .linkDistance(linkDistance * 3)
@@ -246,10 +247,10 @@ var graph = (function (d3) {
       .on("tick", fociTick)
       .on("start", function () {
         bubbles.transition()
-          .duration(2350)
+          .duration(2000)
           // .ease("circle")
           .attrTween("r", function (d) {
-            var i = d3.interpolate(5, d.r);
+            var i = d3.interpolate(1, d.r);
             return function (t) { return d.r = i(t); };
           })
       })
@@ -522,7 +523,7 @@ var graph = (function (d3) {
       });
     }
 
-    var networkVisible = false;
+
     vis.style("visibility", null);
     function fociTick (e) {
       var q = d3.geom.quadtree(nodes),
@@ -530,6 +531,10 @@ var graph = (function (d3) {
         n = nodes.length;
 
       while (++i < n) q.visit(collide(nodes[i]));
+
+      if ((+new Date()) - startTick > symTime) {
+        force.stop();
+      }
 
       // if (e.alpha < alphaThreshold && !networkVisible) {
       //   vis.style("visibility", null);
@@ -653,7 +658,8 @@ directive("mvGraph",
 
         function filterNodes(nodes, pattern) {
           return nodes.filter(function (n) {
-            return n.description && n.description.search(pattern) > -1;
+            return n.type === "gene" ||
+                   (n.description && n.description.search(pattern) > -1);
           });
         }
 
